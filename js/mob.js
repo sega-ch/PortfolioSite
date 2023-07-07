3969dd0262b545b96a2ca23f5ec9fc7ebabccd58
serg <59026328+sega-ch@users.noreply.github.com>
2023-07-07 13:54:00 +0500

    Update mob.js

--- js/mob.js
+++ js/mob.js
@@ -7,13 +7,13 @@ brg.addEventListener("click", function () {
 
     if (isOpen) {
 
-        header.style = "transform: translateX(-350px); transition: transform .5s";
+        header.style = "transform: translateX(-500px); transition: transform .5s";
         line[1].style = "opacity: 1; transform: translateY(10px); transition: transform .5s";
         line[0].style = " transform: rotate(0deg) translateY(0px); transition: transform .5s";
         line[2].style = " transform: rotate(0deg)  translateY(20px); transition: transform .5s";
         isOpen = false;
     } else {
-        header.style = "transform: translateX(350px); transition: transform .5s";
+        header.style = "transform: translateX(500px); transition: transform .5s";
         line[1].style = "opacity: 0";
         line[0].style = " transform: rotate(45deg); transition: transform .5s";
         line[2].style = " transform: rotate(-45deg); transition: transform .5s";
