6b0d7292236da769c46e33ec94bcbb16d4317a4e
serg <59026328+sega-ch@users.noreply.github.com>
2023-07-21 13:19:55 +0500

    Update mob.js

--- js/mob.js
+++ js/mob.js
@@ -1,7 +1,6 @@
 const brg = document.querySelector(".burger-btn");
 const line = document.querySelectorAll(".line");
 const header = document.querySelector(".header");
-const link = document.querySelector(".link");
 let isOpen = false;
 
 brg.addEventListener("click", function () {
@@ -25,10 +24,4 @@ brg.addEventListener("click", function () {
 
 })
 
-link.addEventListener("click", function () {
-    line[1].style = "opacity: 1; transform: translateY(10px); transition: transform .5s";
-    line[0].style = " transform: rotate(0deg) translateY(0px); transition: transform .5s";
-    line[2].style = " transform: rotate(0deg)  translateY(20px); transition: transform .5s";
-    isOpen = false;
 
-})
